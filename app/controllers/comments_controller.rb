4b88c355e88e721999514c3ef15160f4fc71dfc1
jackrk <kohn.jack@yahoo.com>
2014-12-03 13:40:46 -0600

    comments ajax submit and UI in navbar

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,27 @@
 class CommentsController < ApplicationController
+
 	def new
 		@comment = Comment.new
+		render :layout => false
 	end
 	
 	def create
 		@comment = Comment.new(comment_params)
- 
-		if @comment.save
-			redirect_to @comment
-		else
-			render 'new'
-  end
+		respond_to do |format|
+			if @comment.save
+			  format.html { redirect_to @comment, notice: 'User was successfully created.' }
+			  format.js   {}
+			  format.json { render json: @comment, status: :created, location: @comment }
+			else
+			  format.html { render action: "new" }
+			  format.json { render json: @comment.errors, status: :unprocessable_entity }
+			end
+		end
   	end
 
   	def show
 	  	@comment = Comment.find(params[:id])
+		render :layout => false
 	end
 
 	def index
